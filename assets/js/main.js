935b8dafd9b680c59c11ffe7b2cf6e9280ff1b75
Daniel <danielboone1990@gmail.com>
2021-02-09 00:30:06 +0100

    FEAT GAME: Set up javascript to determine the size of canvas and display canvas when start button is clicked

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,45 @@ window.onload = function () {
         troughthewall_el.classList.remove('active-gamemode');
     })
 }
+
+function setUpGame() {
+    document.getElementById('info-section').classList.add('invisible');
+    document.getElementById('game-section').classList.remove('invisible');
+    setUpCanvas();
+}
+
+function setUpCanvas() {
+    // Setting up the canvas
+    ctx = document.getElementById("game").getContext("2d");
+    //ctx.canvas.style.background = "grey";
+
+    resizeCanvasToDisplaySize(ctx.canvas);
+}
+
+function resizeCanvasToDisplaySize(canvas) {
+    // look up the size the canvas is being displayed and multiply by percentage of canvas size
+
+    const width = (window.innerWidth * 0.9)
+    const height = (window.innerHeight * 0.6)
+    const max_width = 510
+    const max_height = 510;
+
+    // make it dividable by 15 
+
+    var ratioW = Math.floor(width / 15)
+    var ratioH = Math.floor(height / 15)
+
+    // determine the canvas size
+
+    canvas.width = chooseLower((15 * ratioW), max_width);
+    canvas.height = chooseLower((15 * ratioH), max_height);
+}
+
+//Helper functions
+
+function chooseLower(a, b) {
+    if (a > b) {
+        return b
+    }
+    return a
+}
