d03b11fee6aab06e3205ca4981582ceccccf6f1d
Daniel <danielboone1990@gmail.com>
2021-02-10 22:35:32 +0100

    FEAT SCREEN TRANSITION: adding animation to screen transition function

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -10,3 +10,20 @@ function startupAnimation(){
     tl.to(".animation1", {display:"none", duration: 0.1});
     tl.to(".animation3", {display:"none", duration: 0.1}, "-=0.1");
 }
+
+window.addEventListener('click', function(){
+    tl = gsap.timeline({defaults: {ease:"none"}});
+
+    tl.to(".animation1", {display:"block", borderRadius:"0%", duration: 0.1});
+    tl.to(".animation3", {display:"block", borderRadius:"0%", duration: 0.1}, "-=0.1");
+
+    tl.to(".animation1", {width: "50%", duration: 4});
+    tl.to(".animation3", {width: "50%", duration: 4}, "-=4");
+
+    tl.to(".animation1", {width: "0%", duration: 4});
+    tl.to(".animation3", {width: "0%", duration: 4}, "-=4");
+    tl.to(".animation1", {display:"none", duration: 0.1});
+    tl.to(".animation3", {display:"none", duration: 0.1}, "-=0.1");
+
+})
+
