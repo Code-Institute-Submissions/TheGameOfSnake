9857136bec9b14ed2ab50a22af2443cd726c20d2
Daniel <danielboone1990@gmail.com>
2021-02-14 17:19:53 +0100

    FEAT SCREEN TRANSITION: comment out the click event animation

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -11,19 +11,17 @@ function startupAnimation(){
     tl.to(".animation3", {display:"none", duration: 0.1}, "-=0.1");
 }
 
-window.addEventListener('click', function(){
-    tl = gsap.timeline({defaults: {ease:"none"}});
+// window.addEventListener('click', function(){
+//     tl = gsap.timeline({defaults: {ease:"none"}});
 
-    tl.to(".animation1", {display:"block", borderRadius:"0%", duration: 0.1});
-    tl.to(".animation3", {display:"block", borderRadius:"0%", duration: 0.1}, "-=0.1");
+//     tl.to(".animation1", {display:"block", borderRadius:"0%", duration: 0.1});
+//     tl.to(".animation3", {display:"block", borderRadius:"0%", duration: 0.1}, "-=0.1");
 
-    tl.to(".animation1", {width: "50%", duration: 4});
-    tl.to(".animation3", {width: "50%", duration: 4}, "-=4");
+//     tl.to(".animation1", {width: "50%", duration: 4});
+//     tl.to(".animation3", {width: "50%", duration: 4}, "-=4");
 
-    tl.to(".animation1", {width: "0%", duration: 4});
-    tl.to(".animation3", {width: "0%", duration: 4}, "-=4");
-    tl.to(".animation1", {display:"none", duration: 0.1});
-    tl.to(".animation3", {display:"none", duration: 0.1}, "-=0.1");
+//     tl.to(".animation1", {display:"none", duration: 0.1});
+//     tl.to(".animation3", {display:"none", duration: 0.1}, "-=0.1");
 
-})
+// })
 
